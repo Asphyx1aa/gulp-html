637aa473e6ffcdce3efe79b4a22552e2485641e0
Asphyx1aa <52380054+Asphyx1aa@users.noreply.github.com>
2021-11-24 14:07:00 +0300

    Add files via upload
    gulp build html, css, image minification. fonts

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+const { src, dest, parallel, series } = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const sourcemaps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cleancss = require('gulp-clean-css');
+const ttf2woff = require('gulp-ttf2woff');
+const ttf2woff2 = require('gulp-ttf2woff2');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    },
+    notify: false,
+  })
+}
+
+function clean() {
+    return del('./dist')
+}
+
+function html() {
+    return src('./src/index.html')
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(dest('./dist'))
+      .pipe(browserSync.stream())
+}
+
+function style() {
+  return src('./src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('main.min.css'))
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(cleancss({ level: 2 }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('./dist/css'))
+    .pipe(browserSync.stream())
+}
+
+function fonts() {
+   return src('./src/fonts/*.ttf')
+     .pipe(ttf2woff())
+     .pipe(src('./src/fonts/*.ttf'))
+     .pipe(ttf2woff2())
+     .pipe(dest('./dist/fonts'))
+}
+
+function image() {
+   return src('./src/img/*')
+    .pipe(imagemin())
+    .pipe(dest('./dist/img'))
+}
+
+exports.build = parallel(html, style, image)
+
+exports.html = html;
+exports.style = style;
+exports.fonts = fonts;
+exports.image = image;
+exports.del = clean;
